88024891b9da0a27074b092dbadd58763168dc91
Kent Salcedo <kentsalcedo@Kents-MacBook-Pro.local>
2015-12-11 19:48:57 -1000

    completed canvas, color picker, current color swatch, clear button, eraser button, and css styling. added goldfish favicon.

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -35,37 +35,105 @@ for (var i = 0; i < colorArray.length; i++) {
   pal.addEventListener('click', function(){
     // console.log(colorArray[this.id]);
     // console.log(selectedColor);
+
+    console.log('hello');
+    currentColorSelectedSwatch.style.background = colorArray[this.id];
     return selectedColor = colorArray[this.id];
   });
 }
 
-// var palBoxie = function() {
-//   var palBox = document.createElement('div');
-//     palBox.className = 'palBox';
-//     palBox.id = 'swatch'+counter;
-//     targetSideMenu.appendChild(palBox);
-//     counter++;
-// };
-
-// var j = 0;
-
-// while ( j < 56 ){
-//   palBoxie();
-//   j++;
-// }
-
-
 var targetMainMenu = document.getElementById('main');
 
-for (var i = 0; i < 2000; i++) {
+var currentColorSelectedSwatch = document.createElement('div');
+currentColorSelectedSwatch.innerHTML = 'CURRENT COLOR';
+currentColorSelectedSwatch.className = 'currentColorSelectedSwatch';
+targetSideMenu.appendChild(currentColorSelectedSwatch);
+
+
+for (var i = 0; i < 1316; i++) {
   var pix = document.createElement('div');
-    pix.id = 'pix'+1;
+    pix.id = 'pix'+i;
   targetMainMenu.appendChild(pix);
     pix.className = 'inPixel';
     pix.addEventListener('click', function(){
       this.style.background = selectedColor;
     });
 }
+
+
+var defaultCanvas = function(){
+
+  var blackDefault = [0,1,2,3,43,44,45,46,47,140,141,187,93,94,1128,1174,1175,1221,1222,1268,1269,1270,1271,1272,1312,1313,1314,1315];
+
+  for (var i = 0; i < blackDefault.length; i++) {
+    var getDefault1 = document.getElementById('pix'+blackDefault[i]);
+    getDefault1.style.background = 'black';
+  }
+
+  var silverDefault = [49,91,95,139,1176,1220,1224,1266];
+
+  for (var j = 0; j < silverDefault.length; j++) {
+    var getDefault2 = document.getElementById('pix'+silverDefault[j]);
+    getDefault2.style.background = 'silver';
+  }
+
+  var grayDefault = [48,92,1223,1267];
+
+  for (var k = 0; k < grayDefault.length; k++) {
+    var getDefault3 = document.getElementById('pix'+grayDefault[k]);
+    getDefault3.style.background = 'gray';
+  }
+
+  // console.log('hellooooo');
+
+};
+
+defaultCanvas();
+
+var clearButton = document.createElement('button');
+clearButton.innerHTML = 'CLEAR';
+clearButton.id = 'clearButton';
+
+targetSideMenu.appendChild(clearButton);
+
+// clearButton.addEventListener('click', function(){
+//   console.log('hellooooo');
+//   var selectAllPixels = document.querySelectorAll("div.inPixel");
+//     for (var m = 0; m < 2000; m++) {
+//     selectAllPixels[i].style.background = 'white';
+//   };
+// });
+
+clearButton.addEventListener('click', function(){
+  var selectAllPixels = document.querySelectorAll("div.inPixel");
+  for (var m = 0; m < 2000; m++) {
+    selectAllPixels[m].style.background = 'white';
+  }
+  defaultCanvas();
+});
+
+
+// create erase button
+
+var eraseButton = document.createElement('button');
+eraseButton.innerHTML = 'ERASER';
+eraseButton.id = 'eraserButton';
+
+targetSideMenu.appendChild(eraseButton);
+
+// clearButton.addEventListener('click', function(){
+//   console.log('hellooooo');
+//   var selectAllPixels = document.querySelectorAll("div.inPixel");
+//     for (var m = 0; m < 2000; m++) {
+//     selectAllPixels[i].style.background = 'white';
+//   };
+// });
+
+eraseButton.addEventListener('click', function(){
+  selectedColor = 'white';
+});
+
+
 
 
 // })();
